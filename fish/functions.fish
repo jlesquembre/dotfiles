d7fe5026d611b75a33657ee7739fc1ba127a0855
José Luis Lafuente <jl@lafuente.me>
2015-05-29 17:23:33 +0200

    Install thefuck

--- fish/functions.fish
+++ fish/functions.fish
@@ -139,3 +139,21 @@ end
 #### Blackbox ####
 
 set -x PATH $PATH $here/extras/blackbox/bin
+
+
+## https://github.com/nvbn/thefuck
+#### thefuck ####
+
+set -x THEFUCK_REQUIRE_CONFIRMATION true
+
+function fuck --description 'Correct your previous console command'
+    set -l exit_code $status
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    set -l fucked_up_commandd $history[1]
+    thefuck $fucked_up_commandd > $eval_script
+    . $eval_script
+    rm $eval_script
+    if test $exit_code -ne 0
+        history --delete $fucked_up_commandd
+    end
+end
