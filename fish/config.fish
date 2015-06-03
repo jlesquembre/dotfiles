a6baa168c1952286c6c7c1c66f32191829fba0c3
José Luis Lafuente <jl@lafuente.me>
2015-06-03 09:58:32 +0200

    Add fish abbreviation for `git fetch`

--- fish/config.fish
+++ fish/config.fish
@@ -51,6 +51,7 @@ end
 
 # Some abbreviations
 set -x fish_user_abbreviations $fish_user_abbreviations 'gco=git checkout'
+set -x fish_user_abbreviations $fish_user_abbreviations 'gf=git fetch'
 set -x fish_user_abbreviations $fish_user_abbreviations 'gp=git pull --ff-only'
 set -x fish_user_abbreviations $fish_user_abbreviations 'gd=git branch -d'
 set -x fish_user_abbreviations $fish_user_abbreviations 'gdr=git push origin --delete'
