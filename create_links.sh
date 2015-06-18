2112573716a04f68ff1df34400cea063a6c883fc
José Luis Lafuente <jl@lafuente.me>
2015-06-18 16:27:47 +0200

    Add option to display symlinks

--- create_links.sh
+++ create_links.sh
@@ -18,23 +18,48 @@ links["$HOME/dotfiles/tigrc"]="$HOME/.tigrc"
 
 # key   -> target
 # value -> link
-for target in "${!links[@]}"
-do
-  link=${links[$target]}
 
-  # Check if there is a file with same link name
-  if [ -e "$link" ] || [ -L "$link" ]; then
-    if [ -L "$link" ]; then
-      # It's a symlink
-      rm "$link"
-    elif [ -d "$link" ]; then
-      # It's a directory
-      rm -r "$link"
-    else
-      rm "$link"
+execute_commands() { #{{{
+  for target in "${!links[@]}"
+  do
+    link=${links[$target]}
+
+    # Check if there is a file with same link name
+    if [ -e "$link" ] || [ -L "$link" ]; then
+      if [ -L "$link" ]; then
+        # It's a symlink
+        rm "$link"
+      elif [ -d "$link" ]; then
+        # It's a directory
+        rm -r "$link"
+      else
+        rm "$link"
+      fi
     fi
-  fi
 
-  ln -s "$target" "$link"
+    ln -s "$target" "$link"
 
-done
+  done
+} #}}}
+
+print_commands() { #{{{
+  for target in "${!links[@]}"
+  do
+    link=${links[$target]}
+    echo "ln -s $target $link"
+  done
+} #}}}
+
+
+echo " 1) Print commands [DEFAULT]"
+echo " 2) Execute commands"
+echo ""
+read -p '> ' OPTION
+case "$OPTION" in
+  2)
+    execute_commands
+    ;;
+  *)
+    print_commands
+    ;;
+esac
