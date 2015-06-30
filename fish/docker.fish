df1ed9a71be948d487f7d6de21368416016a27b0
José Luis Lafuente <jl@lafuente.me>
2015-06-30 10:59:01 +0200

    Fix docker helpers

--- fish/docker.fish
+++ fish/docker.fish
@@ -6,6 +6,8 @@ function dockrm_untag  --description 'Remove docker untagged images'
     set image_ids (docker images -q --filter "dangling=true")
     if [ -n "$image_ids" ]
         docker rmi $image_ids
+    else
+        echo "No untagged docker images, nothing to do"
     end
 end
 
@@ -13,9 +15,16 @@ function dockrm_images  --description 'Remove ALL docker images'
     set image_ids (docker images -q)
     if [ -n "$image_ids" ]
         docker rmi $image_ids
+    else
+        echo "No docker images, nothing to do"
     end
 end
 
-function dockrm_containers  --description 'Remove docker stopped containers'
-    docker rm -v (docker ps -aq)
+function dockrm_containers  --description 'Remove docker exited containers'
+    set cont_ids (docker ps -aq -f "status=exited")
+    if count $cont_ids > /dev/null
+        docker rm -v $cont_ids
+    else
+        echo "No exited docker containers, nothing to do"
+    end
 end
