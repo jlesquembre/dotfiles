eaae9e2209918405c68de768918a0ea6cf1d3840
José Luis Lafuente <jl@lafuente.me>
2016-01-27 14:09:03 +0100

    Use pulseaudio-ctl to control the sound level

--- i3/get_volume.sh
+++ i3/get_volume.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-mute=`amixer -D pulse sget Master | grep "Front Left:" | awk '{print $6}'`
 
-if [ $mute == "[off]" ] ; then
+volume=`pulseaudio-ctl full-status`
+level=`echo $volume | awk '{print $1}'`
+out_muted=`echo $volume | awk '{print $2}'`
+in_muted=`echo $volume | awk '{print $3}'`
+
+if [ $out_muted == "yes" ] ; then
     echo "[Mute]"
 else
-    echo `amixer -D pulse sget Master | awk '/Front Left:/ {print $5}' | tr -dc "0-9"`%
+    echo "${level}%"
 fi
 
+
+#mute=`amixer -D pulse sget Master | grep "Front Left:" | awk '{print $6}'`
+#
+#if [ $mute == "[off]" ] ; then
+#    echo "[Mute]"
+#else
+#    echo `amixer -D pulse sget Master | awk '/Front Left:/ {print $5}' | tr -dc "0-9"`%
+#fi
