b2d1f4df27c21c359bb2d29a1078b887b9180da7
José Luis Lafuente <jl@lafuente.me>
2016-10-20 21:37:26 +0200

    Add preview for fzf

--- fish/fzf.fish
+++ fish/fzf.fish
@@ -1,3 +1,11 @@
+set fzf_preview_all '--preview "file -ib {} | rg binary | rg -v inode/ ^ /tmp/null; or pygmentize -O style=monokai -f console256 -g {} ^ /dev/null; or tree -C {} | head -200"'
+set fzf_preview_dir '--preview "tree -C {} | head -200"'
+
+set -x FZF_CTRL_T_OPTS $fzf_preview_all
+set -x FZF_ALT_C_OPTS $fzf_preview_dir
+set -x FZF_CTRL_R_OPTS '--preview "echo {}" --preview-window down:3:hidden --bind "?:toggle-preview"'
+
+
 function _run_fzf_cmd
     set -l tempfile $argv[1]
     set -l cmd $argv[2]
@@ -26,7 +34,7 @@ end
 function fo --description 'open file using fzf and rifle (CTRL-V for vim)'
     # Without args, get all files
     if test (count $argv) -eq 0; set argv '\*'; end
-    set -l cmd "locate -Ai -0 $argv | fzf --read0 -0 -1 --expect=ctrl-v --header='Open file with rifle (CTRL-V for vim)'"
+    set -l cmd "locate -Ai -0 $argv | fzf --read0 -0 -1 --expect=ctrl-v --header='Open file with rifle (CTRL-V for vim)' $fzf_preview_all"
 
     set -l tempfile (mktemp)
     _run_fzf_cmd $tempfile $cmd $argv; set -l last_status $status
@@ -52,7 +60,7 @@ end
 function fcd -d 'fzf cd'
     #locate -Ai -0 $argv | grep -z -vE '~$' | fzf --read0 -0 -1 > $tempfile
     if test (count $argv) -eq 0; set argv '\*'; end
-    set -l cmd "locate -Ai -0 $argv | fzf --read0 -0 -1"
+    set -l cmd "locate -Ai -0 $argv | fzf --read0 -0 -1 $fzf_preview_all"
 
     set -l tempfile (mktemp)
     _run_fzf_cmd $tempfile $cmd $argv; set -l last_status $status
@@ -72,7 +80,7 @@ end
 # Not too useful
 function fcdd --description 'fzf cd (only match basename)'
     if test (count $argv) -eq 0; set argv '\*'; end
-    set -l cmd "locate -Ai -0 -b $argv | fzf --read0 -0 -1"
+    set -l cmd "locate -Ai -0 -b $argv | fzf --read0 -0 -1 $fzf_preview_all"
 
     set -l tempfile (mktemp)
     _run_fzf_cmd $tempfile $cmd $argv; set -l last_status $status
@@ -109,7 +117,7 @@ end
 
 function jj -d 'autojump with fzf'
     if test (count $argv) -eq 0; set argv ''; end
-    set -l cmd "autojump -s | head -n -7 | sort -nr | awk '{print \$2}' | fzf +s --query=\"$argv\""
+    set -l cmd "autojump -s | head -n -7 | sort -nr | awk '{print \$2}' | fzf +s --query=\"$argv\" $fzf_preview_dir"
 
     set -l tempfile (mktemp)
     _run_fzf_cmd $tempfile $cmd; set -l last_status $status
