ca2f5effb69ad2bcab867cdfb7b94cd6a3d8b8d6
José Luis Lafuente <jl@lafuente.me>
2017-01-02 10:37:31 +0100

    fish, add last command duration

--- fish/config.fish
+++ fish/config.fish
@@ -190,3 +190,28 @@ function fish_prompt
     printf " "
 
 end
+
+function humanize_duration -d "Humanize a time interval for display"
+    command awk '
+        function hmTime(time,   stamp) {
+            split("h:m:s:ms", units, ":")
+            for (i = 2; i >= -1; i--) {
+                if (t = int( i < 0 ? time % 1000 : time / (60 ^ i * 1000) % 60 )) {
+                    stamp = stamp t units[sqrt((i - 2) ^ 2) + 1] " "
+                }
+            }
+            if (stamp ~ /^ *$/) {
+                return "0ms"
+            }
+            return substr(stamp, 1, length(stamp) - 1)
+        }
+        {
+            print hmTime($0)
+        }
+    '
+end
+
+function fish_right_prompt -d "Write out the right prompt"
+    set -l last_duration (echo $CMD_DURATION | humanize_duration)
+    printf "[ $last_duration ]"
+end
