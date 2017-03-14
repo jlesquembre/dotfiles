03452ceb433e13cd644fbfe35fc4043b6173bc76
José Luis Lafuente <jl@lafuente.me>
2017-03-14 21:24:13 +0100

    Remove sack, blackbox and RISE
    Delete git submodules

--- fish/functions_extra/functions.fish
+++ fish/functions_extra/functions.fish
@@ -217,27 +217,6 @@ end
 
 set here (dirname (status -f))
 
-##   https://github.com/garaud/sack.git
-#### Sack ####
-
-set -x SACK_EDITOR gvim
-set -x SACK_SHORTCUT /tmp/.sack_shortcuts
-
-function sag --description 's(hortcut)-ag'
-    bash $here/extras/sack/sack -ag $argv
-end
-
-function F --description 'Open s(hortcut)-ag file'
-    bash $here/extras/sack/F $argv
-end
-
-
-##  https://github.com/StackExchange/blackbox.git
-#### Blackbox ####
-
-set -x PATH $PATH $here/extras/blackbox/bin
-
-
 ## https://github.com/nvbn/thefuck
 #### thefuck ####
 
