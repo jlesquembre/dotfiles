6bb2d03ad5c515550415a5d74b9625660aed3bf2
José Luis Lafuente <jl@lafuente.me>
2017-03-24 17:54:33 +0100

    nix, refactor `delta` configuration

--- nix/machines/delta.nix
+++ nix/machines/delta.nix
@@ -1,23 +1,29 @@
-
 { config, pkgs, ... }:
 
 {
 
-  hardware.opengl.driSupport32Bit = true;
-  hardware.bumblebee.enable = true;
-  #services.xserver.videoDrivers = [ "nvidia" ];
-
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.blacklistedKernelModules = [ "nouveau" ];
+
+
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.bumblebee.enable = true;
+  hardware.enableAllFirmware = true;
+
+
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  #services.xserver.videoDrivers = [ "nvidia" "vesa" ];
+  services.xserver.synaptics.enable = true;
+
   #boot.blacklistedKernelModules = [ "i915" ];
   #boot.kernelParams = [ "nomodeset" "video=vesa:off" "vga=normal" ];
   #boot.vesa = false;
 
-  hardware.opengl.enable = true;
-  #hardware.opengl.driSupport32Bit = true;
-  #services.xserver.videoDrivers = [ "nvidia" "vesa" ];
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.networks = import ../wireless-networks.nix {};
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 }
