45642d5a04d17a64337d8b63a3a338f2db483e38
José Luis Lafuente <jl@lafuente.me>
2017-03-27 20:02:20 +0200

    nix, better wifi options

--- nix/machines/delta.nix
+++ nix/machines/delta.nix
@@ -24,6 +24,8 @@
   #boot.vesa = false;
 
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.userControlled.enable = true;
   networking.wireless.networks = import ../wireless-networks.nix {};
+  environment.systemPackages = [ pkgs.wpa_supplicant_gui ];
 
 }
