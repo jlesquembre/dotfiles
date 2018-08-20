2195c73101409d09e9f1d1cb940feb586bf26716
José Luis Lafuente <jl@lafuente.me>
2018-08-20 13:46:08 +0200

    fish, remove depth from clone function

--- fish/functions_extra/functions.fish
+++ fish/functions_extra/functions.fish
@@ -255,7 +255,7 @@ end
 #end
 
 function clone --description "clone something, cd into it. install it."
-  git clone --depth=1 $argv[1]
+  git clone $argv[1]
   cd (basename $argv[1] | sed 's/.git$//')
   # yarn install
 end
