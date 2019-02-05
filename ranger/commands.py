8fc1d55a518bf2be5db64e04d5f80139fd5d383d
José Luis Lafuente <jl@lafuente.me>
2019-02-05 22:41:27 +0100

    ranger: add custom commands

---
+++ ranger/commands.py
@@ -0,0 +1,38 @@
+from ranger.api.commands import Command
+
+import os
+from ranger.core.loader import CommandLoader
+
+class compress(Command):
+    def execute(self):
+        """ Compress marked files to current directory """
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+    def tab(self, tabnum):
+        """ Complete with current folder name """
+
+        extension = [
+            '.tar.gz',
+            '.7z',
+            '.zip',
+        ]
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
