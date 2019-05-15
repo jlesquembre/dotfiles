9b140ce726675639353d0e70fc9e86622d23f4ae
José Luis Lafuente <jl@lafuente.me>
2019-05-15 15:17:58 +0200

    nix: better hardware configuration

--- nix/machines/beta.nix
+++ nix/machines/beta.nix
@@ -5,6 +5,18 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "i915" ];
+
+  # https://github.com/NixOS/nixos-hardware/blob/master/common/cpu/intel/kaby-lake/default.nix
+  # boot.kernelParams = [
+  #   "i915.enable_fbc=1"
+  #   "i915.enable_psr=2"
+  #   # "i915.enable_guc=2"
+  # ];
+
+  # https://github.com/NixOS/nixos-hardware/blob/master/common/pc/laptop/acpi_call.nix
+  boot.kernelModules = [ "acpi_call" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
 
   # Enable touchpad, for options
   # see https://nixos.org/nixos/options.html#libinput
@@ -12,8 +24,19 @@
 
   hardware.cpu.intel.updateMicrocode = true;
 
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  # https://nixos.wiki/wiki/Accelerated_Video_Playback
+  hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver
+    ];
+  };
+
+
   hardware.enableAllFirmware = true;
 
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
