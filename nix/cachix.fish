12b56e0ea972908659d3ad40d064aae8f406121b
José Luis Lafuente <jl@lafuente.me>
2019-10-15 12:59:09 +0200

    nix: update cachix server

--- nix/cachix.fish
+++ nix/cachix.fish
@@ -4,8 +4,8 @@ cd /tmp
 set -l packages nerdfonts neovim cdrtools texlive.combined.scheme-full # graalvm8 purescript psc-package libreoffice-fresh
 
 for package in $packages
-  nix-build "<nixpkgs>" -A $package | cachix push jl
+  nix-build "<nixpkgs>" -A $package | cachix push jl-test
 end
 
-nix-build ~/dotfiles/nix/emacs.nix | cachix push jl
-nix-build ~/dotfiles/nix/vscode.nix | cachix push jl
+nix-build ~/dotfiles/nix/emacs.nix | cachix push jl-test
+nix-build ~/dotfiles/nix/vscode.nix | cachix push jl-test
