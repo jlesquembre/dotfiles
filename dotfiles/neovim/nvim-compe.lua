afda2a48feb603b9b7f0458821d4b27922d16612
José Luis Lafuente <jl@lafuente.me>
2021-04-20 16:04:59 +0200

    nvim: typo in config

--- dotfiles/neovim/nvim-compe.lua
+++ dotfiles/neovim/nvim-compe.lua
@@ -98,7 +98,7 @@ vim.g.completion_confirm_key = ""
 MUtils.completion_confirm = function()
   if vim.fn.pumvisible() ~= 0  then
     if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc("<c-r>"))
+      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
     else
       return npairs.esc("<cr>")
     end
