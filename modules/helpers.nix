b790502c0f746fc508042e2083707fb55862e39b
José Luis Lafuente <jl@lafuente.me>
2021-09-02 19:37:57 +0200

    nix-medley

--- modules/helpers.nix
+++ modules/helpers.nix
@@ -1,6 +1,16 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  nix-medley = (import ../../projects/nix-medley { inherit pkgs; });
+  # nix-medley = (import ../../projects/nix-medley { inherit pkgs; });
+  nix-medley =
+    let
+      commit = "342e8521009b7ec814a68bdde3008561029915f6";
+      src =
+        fetchTarball {
+          url = "https://github.com/jlesquembre/nix-medley/archive/${commit}.tar.gz";
+          sha256 = "05m6dj3rj8j8qh8jn8hh0j7sh8x56gb8my450cc87fxr3gidwkcg";
+        };
+    in
+    (import src { inherit pkgs; });
 in
 {
   import-secret = secretPath: (builtins.exec [
