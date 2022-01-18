a33cd25e42bd027e0b0eee9241b959ae84273fbf
José Luis Lafuente <jl@lafuente.me>
2022-01-18 19:04:08 +0100

    nvim: update commands to 0.6

--- dotfiles/neovim/lspconfig.lua
+++ dotfiles/neovim/lspconfig.lua
@@ -63,7 +63,7 @@ local function custom_attach(client, bufnr)
   -- set_keymap('n', 'g0',    '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
   -- set_keymap('n', 'gW',    '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
 
-  set_keymap('n', '<leader>dd',  [[<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border = "single"})<CR>]], opts)
+  set_keymap('n', '<leader>dd',  [[<cmd>lua vim.diagnostic.open_float({border = "single"})<CR>]], opts)
   set_keymap('n', '[w', [[<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts={border="single"}})<CR>]], opts)
   set_keymap('n', ']w', [[<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts={border="single"}})<CR>]], opts)
 
