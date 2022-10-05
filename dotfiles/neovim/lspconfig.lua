5e618ac6bea65ea6f06f85dc140de86078c4427e
José Luis Lafuente <jl@lafuente.me>
2022-10-05 17:18:19 +0200

    format lua code

--- dotfiles/neovim/lspconfig.lua
+++ dotfiles/neovim/lspconfig.lua
@@ -5,72 +5,73 @@
 -- See:
 -- https://rishabhrd.github.io/jekyll/update/2020/09/19/nvim_lsp_config.html
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = true,
-    signs = false,
-    update_in_insert = false,
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  virtual_text = true,
+  signs = false,
+  update_in_insert = false,
 })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
- vim.lsp.handlers.hover, { border = "single" })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
- vim.lsp.handlers.signature_help, { border = "single" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 
-require'goto-preview'.setup{default_mappings=false}
-local lspconfig = require'lspconfig'
+require("goto-preview").setup({ default_mappings = false })
+local lspconfig = require("lspconfig")
 local root_pattern = lspconfig.util.root_pattern
 
 local set_telescope_keymap = function(bufnr, mod, lhs, rhs, opts)
-  vim.api.nvim_buf_set_keymap(bufnr, mod, lhs, "<cmd>lua require('telescope.builtin')."..  rhs .. "<cr>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, mod, lhs, "<cmd>lua require('telescope.builtin')." .. rhs .. "<cr>", opts)
 end
 
 local function preview_location_callback(_, method, result)
   if result == nil or vim.tbl_isempty(result) then
-    vim.lsp.log.info(method, 'No location found')
+    vim.lsp.log.info(method, "No location found")
     return nil
   end
   if vim.tbl_islist(result) then
-    vim.lsp.util.preview_location(result[1], {border = "single"})
+    vim.lsp.util.preview_location(result[1], { border = "single" })
   else
-    vim.lsp.util.preview_location(result, {border = "single"})
+    vim.lsp.util.preview_location(result, { border = "single" })
   end
 end
 
 function _G.peek_definition()
   local params = vim.lsp.util.make_position_params()
-  return vim.lsp.buf_request(0, 'textDocument/definition', params, preview_location_callback)
+  return vim.lsp.buf_request(0, "textDocument/definition", params, preview_location_callback)
 end
 
 local function custom_attach(client, bufnr)
-  local function set_keymap_t(...) set_telescope_keymap(bufnr, ...) end
-  local function set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = {noremap = true, silent = false}
+  local function set_keymap_t(...)
+    set_telescope_keymap(bufnr, ...)
+  end
+  local function set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local opts = { noremap = true, silent = false }
 
-  set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  set_keymap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+  set_keymap("n", "<c-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 
   -- set_keymap('n', 'gdd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   -- set_keymap('n', 'gdi',   '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  set_keymap('n', 'gdd', [[<cmd>lua require('goto-preview').goto_preview_definition()<CR>]], opts)
-  set_keymap('n', 'gdi', [[<cmd>lua require('goto-preview').goto_preview_implementation()<CR>]], opts)
-  set_keymap('n', 'gdc', [[<cmd>lua require('goto-preview').close_all_win()<CR>]], opts)
+  set_keymap("n", "gdd", [[<cmd>lua require('goto-preview').goto_preview_definition()<CR>]], opts)
+  set_keymap("n", "gdi", [[<cmd>lua require('goto-preview').goto_preview_implementation()<CR>]], opts)
+  set_keymap("n", "gdc", [[<cmd>lua require('goto-preview').close_all_win()<CR>]], opts)
   -- set_keymap('n', 'gd',    '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  set_keymap('n', '1gD',   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  set_keymap("n", "1gD", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   -- set_keymap('n', 'gr',    '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   -- set_keymap('n', 'g0',    '<cmd>lua vim.lsp.buf.document_symbol()<CR>', opts)
   -- set_keymap('n', 'gW',    '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>', opts)
 
-  set_keymap('n', '<leader>dd',  [[<cmd>lua vim.diagnostic.open_float({border = "single"})<CR>]], opts)
-  set_keymap('n', '[w', [[<cmd>lua vim.diagnostic.goto_prev({float={border="single"}})<CR>]], opts)
-  set_keymap('n', ']w', [[<cmd>lua vim.diagnostic.goto_next({float={border="single"}})<CR>]], opts)
+  set_keymap("n", "<leader>dd", [[<cmd>lua vim.diagnostic.open_float({border = "single"})<CR>]], opts)
+  set_keymap("n", "[w", [[<cmd>lua vim.diagnostic.goto_prev({float={border="single"}})<CR>]], opts)
+  set_keymap("n", "]w", [[<cmd>lua vim.diagnostic.goto_next({float={border="single"}})<CR>]], opts)
 
-  set_keymap('n', '<leader>rn',  '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 
   -- set_keymap('n', 'gdp',  [[<cmd>lua peek_definition()<CR>]], opts)
-  set_keymap('n', 'gdg', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  set_keymap("n", "gdg", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   --
   -- TELESCOPE
   --
@@ -78,14 +79,13 @@ local function custom_attach(client, bufnr)
   set_keymap_t("n", "<leader>dj", "lsp_document_symbols()", opts)
   set_keymap_t("n", "<leader>dk", "lsp_dynamic_workspace_symbols()", opts)
   set_keymap_t("n", "<leader>dc", "lsp_code_actions()", opts)
-  set_keymap  ("v", "<leader>dc", [[<cmd>'<,'>lua require("telescope.builtin").lsp_code_action()<CR>]], opts)
+  set_keymap("v", "<leader>dc", [[<cmd>'<,'>lua require("telescope.builtin").lsp_code_action()<CR>]], opts)
   set_keymap_t("n", "<leader>dw", "lsp_document_diagnostics()", opts)
   set_keymap_t("n", "<leader>dW", "lsp_workspace_diagnostics()", opts)
 
-
-
   if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
+    vim.api.nvim_exec(
+      [[
       augroup lsp_document_highlight
         autocmd!
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
@@ -93,52 +93,61 @@ local function custom_attach(client, bufnr)
         autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
         autocmd CursorMovedI <buffer> lua vim.lsp.buf.clear_references()
       augroup END
-    ]], false)
+    ]],
+      false
+    )
   end
 end
 
 local function jdt_on_attach(client, bufnr)
-  require'jdtls'.setup_dap()
+  require("jdtls").setup_dap()
   custom_attach(client, bufnr)
 
-  local function set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = {noremap = true, silent = true}
+  local function set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local opts = { noremap = true, silent = true }
 
-  set_keymap('n', '<leader>dc', '<cmd>lua require("jdtls").code_action()<CR>', opts)
-  set_keymap('v', '<leader>dc', '<esc><cmd>lua require("jdtls").code_action(true)<CR>', opts)
+  set_keymap("n", "<leader>dc", '<cmd>lua require("jdtls").code_action()<CR>', opts)
+  set_keymap("v", "<leader>dc", '<esc><cmd>lua require("jdtls").code_action(true)<CR>', opts)
   -- set_keymap('n', '<leader>rn', '<cmd>lua require("jdtls").code_action(false, "refactor")<CR>', opts)
 
-  set_keymap('n', '<leader>di', '<Cmd>lua require"jdtls".organize_imports()<CR>', opts)
-  set_keymap('n', '<leader>de', '<Cmd>lua require("jdtls").extract_variable()<CR>', opts)
-  set_keymap('v', '<leader>de', '<Esc><Cmd>lua require("jdtls").extract_variable(true)<CR>', opts)
-  set_keymap('v', '<leader>dm', '<Esc><Cmd>lua require("jdtls").extract_method(true)<CR>', opts)
+  set_keymap("n", "<leader>di", '<Cmd>lua require"jdtls".organize_imports()<CR>', opts)
+  set_keymap("n", "<leader>de", '<Cmd>lua require("jdtls").extract_variable()<CR>', opts)
+  set_keymap("v", "<leader>de", '<Esc><Cmd>lua require("jdtls").extract_variable(true)<CR>', opts)
+  set_keymap("v", "<leader>dm", '<Esc><Cmd>lua require("jdtls").extract_method(true)<CR>', opts)
 
   -- Run test mappings
-  set_keymap('n', 'cptt', '<Cmd>lua require"jdtls".test_nearest_method()<CR>', opts)
-  set_keymap('n', 'cpta', '<Cmd>lua require"jdtls".test_class()<CR>', opts)
+  set_keymap("n", "cptt", '<Cmd>lua require"jdtls".test_nearest_method()<CR>', opts)
+  set_keymap("n", "cpta", '<Cmd>lua require"jdtls".test_class()<CR>', opts)
 
-  require('jdtls.setup').add_commands()
+  require("jdtls.setup").add_commands()
 
-  vim.api.nvim_exec([[
+  vim.api.nvim_exec(
+    [[
     augroup FormatLspAutogroup
       autocmd!
       autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
     augroup END
-  ]], false)
+  ]],
+    false
+  )
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 local function setup_lspconfig()
-
-  local default_config = {on_attach = custom_attach,
-                          capabilities = capabilities}
+  local default_config = { on_attach = custom_attach, capabilities = capabilities }
   local function extra_config(t)
     r = {}
-    for k,v in pairs(default_config) do r[k] = v end
-    for k,v in pairs(t) do r[k] = v end
+    for k, v in pairs(default_config) do
+      r[k] = v
+    end
+    for k, v in pairs(t) do
+      r[k] = v
+    end
     return r
   end
 
@@ -158,22 +167,20 @@ local function setup_lspconfig()
 
   -- Special configs
   lspconfig.jsonls.setup(extra_config({
-      settings = {
-        json = {
-          schemas = require('schemastore').json.schemas(),
-        },
-      }
+    settings = {
+      json = {
+        schemas = require("schemastore").json.schemas(),
+      },
+    },
   }))
-  lspconfig.tsserver.setup(extra_config(
-                           {root_dir = function(fname)
-                                         return root_pattern("package.json", "tsconfig.json", ".git")(fname) or
-                                                root_pattern(".")(fname)
-                                       end;
-                           }))
+  lspconfig.tsserver.setup(extra_config({
+    root_dir = function(fname)
+      return root_pattern("package.json", "tsconfig.json", ".git")(fname) or root_pattern(".")(fname)
+    end,
+  }))
 end
 
 setup_lspconfig()
-
 
 function start_jdtls()
   local settings = {
@@ -197,41 +204,44 @@ function start_jdtls()
       -- },
       sources = {
         organizeImports = {
-          starThreshold = 9999;
-          staticStarThreshold = 9999;
+          starThreshold = 9999,
+          staticStarThreshold = 9999,
         },
       },
       codeGeneration = {
         toString = {
-          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}"
-        }
+          template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+        },
       },
     },
   }
   local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
-  local root_dir = root_pattern('.git', 'gradlew', 'mvnw', 'pom.xml')(bufname)
+  local root_dir = root_pattern(".git", "gradlew", "mvnw", "pom.xml")(bufname)
   local workspace_dir = "/tmp/jdtls_workspaces/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
   local bundles = {
-    "@java.debug.plugin@"
-  };
+    "@java.debug.plugin@",
+  }
   vim.list_extend(bundles, vim.split(vim.fn.glob("/home/jlle/tmp/vscode-java-test/server/*.jar"), "\n"))
 
-  require('jdtls').start_or_attach({
-      cmd = {'jdt-ls', '-data', workspace_dir},
-      on_attach = jdt_on_attach,
-      root_dir = root_dir,
-      capabilities = capabilities,
-      settings = settings,
-      init_options = {
-        bundles = bundles,
-        extendedCapabilities = require('jdtls').extendedClientCapabilities,
-      },
-    })
+  require("jdtls").start_or_attach({
+    cmd = { "jdt-ls", "-data", workspace_dir },
+    on_attach = jdt_on_attach,
+    root_dir = root_dir,
+    capabilities = capabilities,
+    settings = settings,
+    init_options = {
+      bundles = bundles,
+      extendedCapabilities = require("jdtls").extendedClientCapabilities,
+    },
+  })
 end
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
 augroup LspCustom
   autocmd!
   autocmd FileType java lua start_jdtls()
 augroup END
-]], true)
+]],
+  true
+)
