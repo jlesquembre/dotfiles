961f225e57973b6ce113acfbd2b104e2c4ac16da
José Luis Lafuente <jl@lafuente.me>
2023-11-12 16:04:37 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -21,7 +21,8 @@
     };
 
     githud = {
-      url = "github:gbataille/gitHUD";
+      url = "github:gbataille/gitHUD/be26badf0d7c5dbecaf39de5b2d07660cb1eea20";
+      # url = "github:gbataille/gitHUD";
     };
 
     # wired = {
